17bdc0f3ac4891b0322f8789dfad11e641a0ac05
Marc Reicher <msreicher@gmail.com>
2015-12-04 15:15:30 -0800

    [feature] create express server and add packageJSON and gitIgnore files to project

---
+++ server/server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var path = require('path');
+var bodyParser  = require('body-parser');
+var app = express();
+
+app.use(bodyParser.json());
+
+//sets the static directory as the client directory
+//this may change depending on the location of our index.html file
+app.use(express.static(path.join(__dirname,'/../client')));
+
+app.listen(3000);
+module.exports = app;
