c0afaa9b17482eaa66cc7f3c5f81c9fd95d9c925
Marc Reicher <msreicher@gmail.com>
2015-12-07 15:31:24 -0800

    [feature] github authentication functional, need to modularize and clean up code

---
+++ secrets.js
@@ -0,0 +1,6 @@
+module.exports = function() {
+  return {
+    GITHUB_CLIENT_ID: "a6dbfd60687f250f6ecf",
+    GITHUB_CLIENT_SECRET: "1c63e33c62104235fb4bab21039295656172ef82"
+  }
+}
--- server/server.js
+++ server/server.js
@@ -1,26 +1,20 @@
 var express = require('express');
 var React = require('react');
 var Router = require('react-router');
+var passport = require('passport');
+var session = require('express-session');
+var GitHubStrategy = require('passport-github2').Strategy;
 var path = require('path');
 var bodyParser  = require('body-parser');
 var app = express();
 //requires file that connects to database
 var db = require('./database/dbconfig.js');
+var authRouter = express.Router();
+var User = db.User;
 
-app.use(bodyParser.json());
-
-//sets the static directory as the client directory
-//this may change depending on the location of our index.html file
-app.use(express.static(path.join(__dirname,'/../client')));
 
 var routes from "../shared/routes";
 
-// app.get('/*', function (req, res) {  
-//   Router.run(routes, req.url, Handler => {
-//     let content = React.renderToString(<Handler />);
-//     res.render('index', { content: content });
-//   });
-// });
 
 app.listen(3000);
 module.exports = app;
