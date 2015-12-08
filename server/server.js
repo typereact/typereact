2f1c3fc2b4893e863affa35c8bff047ea7d0684a
Marc Reicher <msreicher@gmail.com>
2015-12-07 17:00:32 -0800

    [feature] github authentication modularized

--- server/server.js
+++ server/server.js
@@ -1,20 +1,30 @@
 var express = require('express');
 var React = require('react');
 var Router = require('react-router');
+var path = require('path');
 var passport = require('passport');
 var session = require('express-session');
 var GitHubStrategy = require('passport-github2').Strategy;
-var path = require('path');
 var bodyParser  = require('body-parser');
 var app = express();
-//requires file that connects to database
 var db = require('./database/dbconfig.js');
-var authRouter = express.Router();
 var User = db.User;
 
 
 var routes from "../shared/routes";
 
+var authRouter = require('./routers/authRouter.js')(express, passport, User, GitHubStrategy);
+
+app.use(bodyParser.json());
+app.use(session({ secret: 'typereact secret' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+//sets the static directory as the client directory
+//this may change depending on the location of our index.html file
+app.use(express.static(path.join(__dirname,'/../client')));
+
+app.use('/auth', authRouter);
 
 app.listen(3000);
 module.exports = app;
