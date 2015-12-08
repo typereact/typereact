6cd78eda8def8c19ba9a23f660c43ede21f78a9b
Marc Reicher <msreicher@gmail.com>
2015-12-07 18:57:43 -0800

    [cleanup] update secrets file

--- secrets.js
+++ secrets.js
@@ -1,6 +1,8 @@
 module.exports = function() {
   return {
-    GITHUB_CLIENT_ID: "a6dbfd60687f250f6ecf",
-    GITHUB_CLIENT_SECRET: "1c63e33c62104235fb4bab21039295656172ef82"
+    //insert client ID
+    GITHUB_CLIENT_ID: "",
+    //insert secret
+    GITHUB_CLIENT_SECRET: ""
   }
 }
