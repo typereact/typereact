a8c654ef2830f71be59dd0d4c8a3804a37c9a995
Tim Lai <timothy.lai@gmail.com>
2015-12-08 16:31:42 -0800

    [feature] editor redux

--- shared/components/editor.js
+++ shared/components/editor.js
@@ -35,9 +35,94 @@ let Editor = React.createClass({
             tabSize: 2,
             showCursorWhenSelecting: true
         };
+        console.log('inside render function');
         return <Codemirror ref="editor" value={this.state.code} onChange={this.updateCode} options={options} />
     }
 });
+
+/* ACTION CREATOR */
+
+/*
+ * action types
+ */
+
+export const KEY_PRESSED = 'KEY_PRESSED'
+export const STRING_CHANGED = 'STRING_CHANGED'
+
+/*
+ * action creators
+ */
+
+export function keyPressed(ch) {
+    return { type: KEY_PRESSED, ch }
+}
+
+export function stringChanged(string) {
+    return { type: STRING_CHANGED, string }
+}
+
+
+/* REDUCER */
+
+// import { combineReducers } from 'redux'
+// import { KEY_PRESSED, STRING_CHANGED } from './actions'
+
+function trackKeys(state = [], action) {
+  switch (action.type) {
+    case KEY_PRESSED:
+      return [
+        
+      ]
+    default:
+      return state
+  }
+}
+
+function matchCode(state = [], action) {
+  switch (action.type) {
+    case STRING_CHANGED:
+      return [
+        
+      ]
+    default:
+      return state
+  }
+}
+ 
+function todos(state = [], action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return [
+        ...state,
+        {
+          text: action.text,
+          completed: false
+        }
+      ]
+    case COMPLETE_TODO:
+      return [
+        ...state.slice(0, action.index),
+        Object.assign({}, state[action.index], {
+          completed: true
+        }),
+        ...state.slice(action.index + 1)
+      ]
+    default:
+      return state
+  }
+}
+
+const editorApp = combineReducers({
+  trackKeys,
+  matchCode
+})
+
+// export default editorApp
+
+
+
+
+
 
 export default Editor;
 
