3dee56f7397dd157ddd370e65149a4b0506d52aa
Nick Krein <nkreinmusic@gmail.com>
2015-12-12 14:26:10 -0800

    [cleanup] remove cursor and unnecessary options

--- shared/components/solutionEditor.js
+++ shared/components/solutionEditor.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Codemirror from 'react-codemirror';
-// require('react-codemirror/node_modules/codemirror/mode/javascript/javascript.js');
-// require('react-codemirror/node_modules/codemirror/mode/xml/xml');
-// require('react-codemirror/node_modules/codemirror/mode/markdown/markdown');
-// require('react-codemirror/node_modules/codemirror/keymap/sublime.js');
 require('codemirror/lib/codemirror.css');
 require('../css/codemirror.css')
 require('codemirror/mode/javascript/javascript');
@@ -12,43 +8,27 @@ require('codemirror/mode/xml/xml');
 require('codemirror/keymap/sublime');
 var beautify = require('js-beautify').js_beautify;
 
-var Defaults = exports.Defaults = {
-    javascript: 'for(var i=0;i < array.length; i++)',
-    markdown: '# Heading\n\nSome **bold** and _italic_ text\nBy [Jed Watson](https://github.com/JedWatson)'
-}
-
-
 let SolutionEditor = React.createClass({
-    getInitialState: function() {
-        return {
-            // code: defaults.markdown,
-            // mode: 'markdown',
-            code: this.props.challengeSolved,
-            readOnly: true,
-            mode: 'javascript'
-        };
-    },
-    componentWillReceiveProps: function(nextProps) {
-      this.setState({
-        code: beautify(nextProps.challengeSolved, {indent_size: 2})
-      })
-    },
-    updateCode: function(newCode) {
-        this.setState({
-            code: newCode
-        });
-    },
-    render: function() {
-        var options = {
-            lineNumbers: true,
-            mode: this.state.mode,
-            readOnly: this.state.readOnly,
-            tabSize: 2,
-            // mode: 'javascript'
-            keyMap: 'sublime'
-        };
-        return <Codemirror ref="solutionEditor" value={this.state.code} onChange={this.updateCode} options={options} />
-    }
+  getInitialState: function() {
+    return {
+      code: this.props.challengeSolved,
+      readOnly: 'nocursor',
+      mode: 'javascript'
+    };
+  },
+  componentWillReceiveProps: function(nextProps) {
+    this.setState({
+      code: beautify(nextProps.challengeSolved, {indent_size: 2})
+    })
+  },
+  render: function() {
+    var options = {
+      lineNumbers: true,
+      mode: this.state.mode,
+      readOnly: this.state.readOnly,
+    };
+    return <Codemirror ref="solutionEditor" value={this.state.code} options={options} />
+  }
 });
 
 export default SolutionEditor;
