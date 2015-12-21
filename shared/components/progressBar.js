7159bd899b4d9cd06759ec83b0408ee152293efe
Marc Reicher <msreicher@gmail.com>
2015-12-21 11:25:32 -0800

    [feature] dynamic progress bar

--- shared/components/progressBar.js
+++ shared/components/progressBar.js
@@ -2,12 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ProgressBar from 'react-bootstrap/lib/ProgressBar.js';
 import { Component } from 'react';
+import { connect } from 'react-redux';
+import editorApp from '../reducers/reducers.js';
+import editDistanceCalculator from '../../editDistanceCalc.js'
 
-export default class GameProgress extends Component {
+class GameProgress extends Component {
+  
   render() {
-  return <div id='progress-bar'><ProgressBar active now={45} /></div>
+    var totalChanges = this.props.editDistance
+    var changesToGo = Math.min(editDistanceCalculator(this.props.code, this.props.solvedCode), totalChanges);
+    var percentProgress = Math.round(((totalChanges - changesToGo) / totalChanges) * 100);
+    return <div id='progress-bar'>Current Progress: {percentProgress}%<ProgressBar active now={percentProgress} /></div>
   }
 }
-    // return <div>HELLO</div>
 
-// export default GameProgress;
+function mapStateToProps(state) {
+  return {
+    code: state.editorState.code,
+    solvedCode: state.editorState.solvedCode,
+    editDistance: state.editorState.editDistance
+  }
+}
+
+export default connect(mapStateToProps)(GameProgress);
